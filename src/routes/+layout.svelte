b382024d32f35baeb7fe18e394a769210792d711
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-12 19:06:38 +0100

    feat(layout): integrate drawer for authenticated users and set page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
+	import Drawer from '$lib/components/drawer.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>RadiusKit</title>
 </svelte:head>
 
-{@render children()}
+<div class="contents h-screen w-full">
+	{#if page.data.user}
+		<Drawer>
+			{@render children()}
+		</Drawer>
+	{:else}
+		{@render children()}
+	{/if}
+</div>
