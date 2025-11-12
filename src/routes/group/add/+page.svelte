c1e670e87cacea8f602e5960181459e18602b4bd
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-12 19:08:58 +0100

    feat(group): add form UI to create new RadGroupCheck group

---
+++ src/routes/group/add/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<h1 class="mb-4 text-2xl font-semibold">Neue RadGroupCheck Gruppe hinzufügen</h1>
+
+<form method="POST" action="?/create" class="space-y-4">
+	<div>
+		<label for="GroupName" class="label">Gruppe</label>
+		<input
+			type="text"
+			name="GroupName"
+			class="input-bordered input w-full"
+			autocomplete="off"
+			required
+		/>
+	</div>
+
+	<div>
+		<label for="Attribute" class="label">Attribut</label>
+		<input type="text" name="Attribute" class="input-bordered input w-full" required />
+	</div>
+
+	<div>
+		<label for="op" class="label">Operator</label>
+		<input type="text" name="op" class="input-bordered input w-20" required />
+	</div>
+
+	<div>
+		<label for="Value" class="label">Wert</label>
+		<input type="text" name="Value" class="input-bordered input w-full" required />
+	</div>
+
+	<div class="flex gap-2">
+		<button type="submit" class="btn btn-primary">Erstellen</button>
+		<a href={resolve('/group')} class="btn btn-secondary">Zurück zur Übersicht</a>
+	</div>
+</form>
