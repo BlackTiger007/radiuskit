5ef7682ed9ff56a5cbcb453d694fc0fb2fbe3e64
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-12 19:10:55 +0100

    feat(group): add server-side logic for editing and deleting RadGroupCheck groups by ID

---
+++ src/routes/group/edit/[id]/+page.server.ts
@@ -0,0 +1,49 @@
+import type { Actions, PageServerLoad } from './$types';
+import { eq } from 'drizzle-orm';
+import { fail, redirect } from '@sveltejs/kit';
+import { radgroupcheck } from '$lib/server/db/schema';
+import { db } from '$lib/server/db';
+import { resolve } from '$app/paths';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const id = Number(params.id);
+
+	const group = await db.select().from(radgroupcheck).where(eq(radgroupcheck.id, id));
+
+	if (!group) throw fail(404, { error: 'Gruppe nicht gefunden' });
+
+	if (group.length > 1) throw fail(500, { error: 'Mehrere Gruppen mit derselben ID gefunden' });
+
+	return { group: group[0] };
+};
+
+export const actions: Actions = {
+	update: async ({ request, params }) => {
+		const form = await request.formData();
+		const id = Number(params.id);
+
+		const groupname = form.get('GroupName') as string;
+		const attribute = form.get('Attribute') as string;
+		const op = form.get('op') as string;
+		const value = form.get('Value') as string;
+
+		if (!groupname || !attribute || !op || !value) {
+			return fail(400, { error: 'Alle Felder sind erforderlich' });
+		}
+
+		await db
+			.update(radgroupcheck)
+			.set({ groupname, attribute, op, value })
+			.where(eq(radgroupcheck.id, id));
+
+		return { success: true };
+	},
+
+	delete: async ({ params }) => {
+		const id = Number(params.id);
+
+		await db.delete(radgroupcheck).where(eq(radgroupcheck.id, id));
+
+		throw redirect(302, resolve('/group'));
+	}
+};
