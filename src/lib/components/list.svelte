e07f46f0513a6eabfe3d7c5c8752e29b3a84a2ce
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-20 12:46:20 +0100

    feat(list): add datalist component for dynamic options rendering

---
+++ src/lib/components/list.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let id: string;
+	export let data: string[] = [];
+</script>
+
+{#if id && data.length > 0}
+	<datalist {id}>
+		{#each data as kst}
+			<option value={kst}>{kst}</option>
+		{/each}
+	</datalist>
+{/if}
