2298b99274e5f68571390499b8567846d4137ffd
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-21 21:07:31 +0100

    feat(config): update database configuration and validation checks

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+// Pflichtvariablen prüfen
+if (
+	!process.env.DB_HOST ||
+	!process.env.DB_PORT ||
+	!process.env.DB_USER ||
+	!process.env.DB_PASSWORD ||
+	!process.env.DB_NAME
+) {
+	throw new Error('Required parameters not set: DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME');
+}
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'mysql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: {
+		host: process.env.DB_HOST,
+		port: Number(process.env.DB_PORT),
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		ssl: {
+			pfx: process.env.DB_SSL_PFX,
+			key: process.env.DB_SSL_KEY,
+			passphrase: process.env.DB_SSL_PASSPHRASE,
+			cert: process.env.DB_SSL_CERT,
+			ca: process.env.DB_SSL_CA,
+			crl: process.env.DB_SSL_CRL,
+			ciphers: process.env.DB_SSL_CIPHERS,
+			rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === 'true'
+		}
+	},
 	verbose: true,
 	strict: true
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,35 @@ import mysql from 'mysql2/promise';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+// Pflichtvariablen prüfen
+if (!env.DB_HOST || !env.DB_PORT || !env.DB_USER || !env.DB_PASSWORD || !env.DB_NAME) {
+	throw new Error(
+		'Es müssen alle Parameter gesetzt sein: DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME'
+	);
+}
 
-const client = mysql.createPool(env.DATABASE_URL);
+// Pool erstellen
+const client = mysql.createPool({
+	host: env.DB_HOST,
+	port: Number(env.DB_PORT),
+	user: env.DB_USER,
+	password: env.DB_PASSWORD,
+	database: env.DB_NAME,
 
-export const db = drizzle(client, { schema, mode: 'default' });
+	// optionale Werte aus ENV
+	waitForConnections: true,
+	connectionLimit: env.DB_POOL_SIZE ? Number(env.DB_POOL_SIZE) : 10,
+	queueLimit: 0,
+
+	// sinnvolle Defaults, individuell anpassbar
+	decimalNumbers: true,
+	supportBigNumbers: true,
+	bigNumberStrings: false,
+	timezone: env.DB_TIMEZONE ?? 'Z'
+});
+
+// Drizzle Client
+export const db = drizzle(client, {
+	schema,
+	mode: 'default'
+});
