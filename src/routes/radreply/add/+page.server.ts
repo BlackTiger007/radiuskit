3e101bd94ad327d32ad9ba45348e94d638efd5f2
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-23 00:14:34 +0100

    Update radreply add action to use typed RadReplayAttribute and provide usernames for form autocomplete

--- src/routes/radreply/add/+page.server.ts
+++ src/routes/radreply/add/+page.server.ts
@@ -1,9 +1,23 @@
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { db } from '$lib/server/db';
-import { radreply, type RadReplyInsert } from '$lib/server/db/schema';
+import { radcheck, radreply, type RadReplyInsert } from '$lib/server/db/schema';
 import { redirect } from '@sveltejs/kit';
 import { resolve } from '$app/paths';
 import type { RadiusOperator } from '$lib/types/operator';
+import type { RadReplayAttribute } from '$lib/types/attribute/radreply';
+
+export const load: PageServerLoad = async () => {
+	const usernamesRaw = await db
+		.select({
+			name: radcheck.username
+		})
+		.from(radcheck);
+
+	// Array von Strings
+	const usernames = usernamesRaw.map((g) => g.name);
+
+	return { usernames: usernames };
+};
 
 export const actions: Actions = {
 	default: async ({ request }) => {
@@ -11,7 +25,7 @@ export const actions: Actions = {
 
 		const data: RadReplyInsert = {
 			username: form.get('username') as string,
-			attribute: form.get('attribute') as string,
+			attribute: form.get('attribute') as RadReplayAttribute,
 			op: (form.get('op') as RadiusOperator) || ':=',
 			value: form.get('value') as string
 		};
