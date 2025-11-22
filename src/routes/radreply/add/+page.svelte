575b38ec562c8353df5751c1d3e992b1c2f5ba87
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-23 00:14:35 +0100

    Add autocomplete and lists for usernames, attributes, and operators in radreply add form

--- src/routes/radreply/add/+page.svelte
+++ src/routes/radreply/add/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import List from '$lib/components/list.svelte';
+	import { RADREPLAY_ATTRIBUTES } from '$lib/types/attribute/radreply';
 	import { RADIUS_OPERATORS } from '$lib/types/operator';
+	import type { PageData } from './$types';
+	export let data: PageData;
 </script>
 
 <h1 class="mb-6 text-3xl font-semibold">Neuen radreply Eintrag hinzufügen</h1>
@@ -9,7 +12,15 @@
 <form method="POST" class="space-y-4">
 	<div class="form-control w-full">
 		<label for="username" class="label">Username</label>
-		<input id="username" type="text" name="username" class="input-bordered input w-full" required />
+		<input
+			id="username"
+			type="text"
+			name="username"
+			class="input-bordered input w-full"
+			list="usernames"
+			autocomplete="off"
+			required
+		/>
 	</div>
 
 	<div class="form-control w-full">
@@ -19,18 +30,34 @@
 			type="text"
 			name="attribute"
 			class="input-bordered input w-full"
+			list="radreplay"
+			autocomplete="off"
 			required
 		/>
 	</div>
 
 	<div>
 		<label for="op" class="label">Operator</label>
-		<input type="text" name="op" class="input-bordered input w-20" list="operator" required />
+		<input
+			type="text"
+			name="op"
+			class="input-bordered input w-20"
+			list="operator"
+			autocomplete="off"
+			required
+		/>
 	</div>
 
 	<div class="form-control w-full">
 		<label for="value" class="label">Value</label>
-		<input id="value" type="text" name="value" class="input-bordered input w-full" required />
+		<input
+			id="value"
+			type="text"
+			name="value"
+			class="input-bordered input w-full"
+			autocomplete="off"
+			required
+		/>
 	</div>
 
 	<div class="mt-4 flex gap-2">
@@ -40,3 +67,5 @@
 </form>
 
 <List id="operator" data={[...RADIUS_OPERATORS]}></List>
+<List id="radreplay" data={[...RADREPLAY_ATTRIBUTES]}></List>
+<List id="usernames" data={[...data.usernames]}></List>
