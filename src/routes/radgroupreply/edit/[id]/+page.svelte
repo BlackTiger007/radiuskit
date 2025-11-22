214b190299610c3c9df72755f9f935b991004c0e
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-22 19:04:39 +0100

    Create edit form page for individual RadGroupReply entries

---
+++ src/routes/radgroupreply/edit/[id]/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import List from '$lib/components/list.svelte';
+	import { RADIUS_OPERATORS } from '$lib/types/operator';
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<h1 class="mb-6 text-3xl font-semibold">radgroupreply bearbeiten: {data.item.groupname}</h1>
+
+<form method="POST" action="?/update" class="space-y-4">
+	<div class="grid grid-cols-2 gap-4">
+		<div class="form-control w-full">
+			<label for="groupname" class="label">Gruppenname</label>
+			<input
+				type="text"
+				name="groupname"
+				class="input-bordered input w-full"
+				value={data.item.groupname}
+			/>
+		</div>
+
+		<div class="form-control w-full">
+			<label for="attribute" class="label">Attribut</label>
+			<input
+				type="text"
+				name="attribute"
+				class="input-bordered input w-full"
+				value={data.item.attribute}
+			/>
+		</div>
+
+		<div>
+			<label for="op" class="label">Operator</label>
+			<input
+				type="text"
+				name="op"
+				class="input-bordered input w-20"
+				list="operator"
+				value={data.item.op}
+				required
+			/>
+		</div>
+
+		<div class="form-control w-full">
+			<label for="value" class="label">Value</label>
+			<input type="text" name="value" class="input-bordered input w-full" value={data.item.value} />
+		</div>
+	</div>
+
+	<div class="mt-4 flex gap-2">
+		<button type="submit" class="btn btn-primary">Speichern</button>
+		<a href={resolve('/radgroupreply')} class="btn">Abbrechen</a>
+	</div>
+</form>
+
+<form method="POST" action="?/delete" class="mt-2">
+	<input type="hidden" name="action" value="delete" />
+	<button type="submit" class="btn btn-error">Löschen</button>
+</form>
+
+<List id="operator" data={[...RADIUS_OPERATORS]}></List>
